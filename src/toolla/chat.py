8c9b99536dbfa7def4ad0f8904f614de2f86960d
George Balch <george.balch@proton.me>
2024-07-23 20:14:07 -0700

    Add OpenAI support

--- src/toolla/chat.py
+++ src/toolla/chat.py
@@ -1,4 +1,5 @@
 import os
+import json
 from typing import List, Dict, Union, Callable
 from pathlib import Path
 from openai import OpenAI
@@ -7,9 +8,11 @@ from anthropic.types.tool_use_block import ToolUseBlock
 from anthropic.types.text_block import TextBlock
 from toolla.utils import (
     build_claude_tool_schema,
+    build_openai_tool_schema,
     get_image_mime_type,
     load_file_base64
 )
+from toolla.models import models, default_tool_prompt
 
 class AnthropicClient:
     def __init__(
@@ -125,18 +128,115 @@ class OpenAIClient:
         model: str = "gpt-4o",
         system: Union[str, None] = None,
         tools: List[Callable] = [],
-        max_steps = 10,
-        print_output=False,
-        api_key = Union[str, None] = None,
+        max_steps: int = 10,
+        print_output: bool = False,
+        api_key: Union[str, None] = None,
     ):
         self.client = OpenAI(api_key=api_key or os.environ.get("OPENAI_API_KEY"))
         self.model = model
-        self.system = system
         self.max_steps = max_steps
         self.messages = []
         self.print_output = print_output
 
-        self.tools = 
+        # TODO change based on model choice
+        self.max_tokens = 4096
+        self.max_chars = 900_000
+
+        if system:
+            self.messages.append(
+                {
+                    "role": "system",
+                    "content": system,
+                }
+            )
+
+        if tools:
+            # TODO check that docstrings
+            # match functions and are valid 
+            self.tools = []
+            self.tool_fns = {}
+            for f in tools:
+                self.tool_fns[f.__name__] = f
+                tool_dict = build_openai_tool_schema(f)
+                self.tools.append(tool_dict)
+        else:
+            self.tools = []
+    
+    def __call__(
+        self,
+        prompt: str,
+        image: Union[str, None] = None,
+        current_fn_response = None,
+        disable_auto_execution = False,
+    ):
+        message = {
+            "role": "user",
+            "content": [
+                { "type": "text", "text": prompt }
+            ]
+        }
+        if image:
+            fpath = Path(image)
+            mtype = get_image_mime_type(fpath)
+            image_string = load_file_base64(fpath) 
+            message["content"].append(
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:{mtype};base64,{image_string}"
+                    }
+                }
+            )
+        self.messages.append(message)
+    
+        while len(str(self.messages)) > self.max_chars:
+            print("TOO BIG")
+            self.messages.pop(0)
+
+        if not self.messages:
+            print("Error: Message is too large for context")
+            return
+        # OpenAI doesn't allow for empty tool list
+        # TODO change to self.tools or None
+        response = self.client.chat.completions.create(
+            model=self.model,
+            max_tokens=self.max_tokens,
+            messages=self.messages,
+            tools=self.tools,
+        ) if self.tools else self.client.chat.completions.create(
+            model=self.model,
+            max_tokens=self.max_tokens,
+            messages=self.messages,
+        )
+ 
+        for choice in response.choices:
+            if choice.finish_reason == 'stop':
+                self.messages.append({
+                    "role": "assistant",
+                    "content": choice.message.content,
+                })
+                if self.print_output:
+                    print(f"{choice.message.content}\n")
+                return current_fn_response
+            elif choice.finish_reason == 'tool_calls':
+                function = choice.message.tool_calls[0].function
+                if disable_auto_execution:
+                    print(f"Function {function.name} is about to be called with inputs: {function.arguments}")
+                    user_input = input("Do you want to run this function? (y/n): ")
+                    if user_input.lower() not in ['y', 'Y']:
+                        print("Function call aborted by user.")
+                        return None
+                r = self.tool_fns[function.name](**json.loads(function.arguments))
+                if len(self.messages) < 2 * self.max_steps:
+                    return self(
+                        prompt=f"\nFunction {function.name} was called and returned a value of {r}",
+                        current_fn_response=r,
+                        disable_auto_execution=disable_auto_execution,
+                    )
+                else:
+                    print("Reached maxiumum number of steps, returning current tool response.")
+                    return current_fn_response
+        return None   
 
 # TODO setup streaming
 class Chat:
@@ -148,27 +248,36 @@ class Chat:
         max_steps = 10,
         print_output=False,
     ):
-        self.client = AnthropicClient(
-            model=model,
-            system=system,
-            tools=tools,
-            max_steps=max_steps,
-            print_output=print_output,
-        )
+        if model in models["openai_models"]:
+            self.client = OpenAIClient(
+                model=model,
+                system=system,
+                tools=tools,
+                max_steps=max_steps,
+                print_output=print_output,
+            )
+        elif model in models["claude_models"]:
+            self.client = AnthropicClient(
+                model=model,
+                system=system,
+                tools=tools,
+                max_steps=max_steps,
+                print_output=print_output,
+            )
 
     def __call__(
         self,
-        prompt: str, 
+        prompt: str,
         image: Union[str, None] = None, # base64 string
         current_fn_response = None,
         disable_auto_execution = False,
     ):
-       return self.client(
+        return self.client(
             prompt=prompt,
             image=image,
             current_fn_response=current_fn_response,
             disable_auto_execution=disable_auto_execution,
-       )
+        )
 
     def get_messages(self):
         return self.client.messages
--- tests/test_chat.py
+++ tests/test_chat.py
@@ -1,25 +1,46 @@
 from toolla.chat import Chat
-from tools import add, multiply, concat
+from .tools import add, multiply, concat
 
-def test_concat_tool():
+def test_openai_add_tool():
+    chat = Chat(model="gpt-4o", tools=[add])
+    r = chat("What is 2+3?")
+    assert r == 5
+
+def test_openai_concat_tool():
+    chat = Chat(model="gpt-4o", tools=[concat])
+    r = chat("Concatenate the strings dream and tremor")
+    assert r == "dreamtremor"
+
+def test_openai_image_content():
+    chat = Chat(model="gpt-4o")
+    chat(prompt="What is this an image of? Answer with one word.", image="./tests/cat.jpg")
+    messages = chat.get_messages()
+    assert 'Cat' in messages[-1]['content'] or 'cat' in messages[-1]['content']
+
+def test_openai_multiple_tools():
+    chat = Chat(model="gpt-4o", tools=[add, multiply])
+    r = chat("What is (4*4911)+18?")
+    assert r == 19662
+
+def test_claude_concat_tool():
     chat = Chat(tools=[concat])
     r = chat("Concatenate the strings dream and tremor")
     assert r == "dreamtremor"
 
-def test_add_tool():
+def test_claude_add_tool():
     chat = Chat(
         tools=[add], 
     )
     r = chat("What is 2+3?")
     assert r == 5
 
-def test_chat_image_content():
+def test_claude_chat_image_content():
     chat = Chat()
     chat(prompt="What is this an image of? Answer with one word.", image="./tests/cat.jpg")
     messages = chat.get_messages()
     assert 'Cat' in messages[-1]['content'] or 'cat' in messages[-1]['content']
 
-def test_multiple_tools():
+def test_claude_multiple_tools():
     chat = Chat(tools=[add, multiply], max_steps=5)
     r = chat("What is (4*4911)+18?")
     assert r == 19662
